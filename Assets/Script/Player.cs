864b723655e7e03d658a6eb034581e79bba2e534
sauliya <sauliya23@gmail.com>
2022-05-15 20:02:52 +0530

    15-0-22

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public Transform groundChecktransform;
+
+    public LayerMask playerMask;
+
+    private bool jumpwaspressed;
+
+    private float horizontalInput;
+
+    private Rigidbody rigidbody;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
+      
         if(Input.GetKeyDown(KeyCode.Space)){
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpwaspressed = true;
         }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+         rigidbody.velocity = new Vector3(horizontalInput, GetComponent<Rigidbody>().velocity.y, 0);
+         
+        if(Physics.OverlapSphere(groundChecktransform.position, 0.01f, playerMask).Length == 0)
+        {
+            return;
+        }
+        if(jumpwaspressed)
+        {
+            rigidbody.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+
+             jumpwaspressed = false;
+        }
+
+       
     }
 }
